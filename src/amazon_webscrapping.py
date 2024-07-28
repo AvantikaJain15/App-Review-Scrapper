06eb35a7cb479130070a60dad726edff69957138
AvantikaJain15 <avantijain15@gmail.com>
2024-07-29 01:01:25 +0530

    Initialize Amazon class with URL, driver, review page URL, dictionary for reviews and a method to open the product link

---
+++ src/amazon_webscrapping.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
+
+class Amazon:
+    def __init__(self, url):
+        self.url =url
+        self.driver = None
+        self.review_page_url = None
+        self.main_dict ={
+            "Star": [],
+            "Summary": [],
+            "Description": []
+        }
+        self.page_count = 0
+    def open_link(self):
+        options =Options()
+        options.page_load_strategy = 'normal'
+        self.driver =webdriver.Chrome(options= options)
+        self.driver.get(sel.url)
+        self.driver.maximize_window()
+        
