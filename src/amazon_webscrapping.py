ff2f67cedd24263f3420c71e598ab9f5f505a07b
AvantikaJain15 <avantijain15@gmail.com>
2024-07-29 22:30:10 +0530

    Add method to navigate to the Amazon product reviews page

--- src/amazon_webscrapping.py
+++ src/amazon_webscrapping.py
@@ -23,4 +23,28 @@ class Amazon:
         self.driver =webdriver.Chrome(options= options)
         self.driver.get(self.url)
         self.driver.maximize_window()
-        
+
+    def get_reviewpage(self):
+        try:
+            self.open_link()
+            review_footer_xpath = "//*[@id='reviews-medley-footer']"
+            review_footer = self.driver.find_element(By.XPATH, review_footer_xpath)
+            review_link = None
+            if review_footer:
+                review_url_xpath = "//*[@id='reviews-medley-footer']/div[2]/a"
+                review_tag = review_footer.find_element(By.XPATH, review_url_xpath)
+                review_link = review_tag.get_attribute("href")
+
+                if review_link:
+                    print(f"INFO | Review Link found.")
+                    print(f"INFO | {review_link}")
+                    self.review_page_url = review_link
+                    print(f"INFO | Opening Review page.")
+                    self.driver.get(self.review_page_url)
+                    return
+                else:
+                    print("ERROR | Review Link not found")
+            else:
+                print("ERROR | Review footer not found")
+        except Exception as e:
+            print(f"ERROR | Review footer not found Error : {e}")
